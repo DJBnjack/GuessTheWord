79033c477c9c87fa0de1297a38c097415ecc01e1
Johannes <johannes.bertens@gmail.com>
2012-04-01 12:47:10 +0000

    second commit

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,8 @@
-var exec = require("child_process").exec;
-
 function start(response) {
   console.log("Request handler 'start' was called.");
-
-  exec("dir", function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout);
-    response.end();
-  });
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Start");
+  response.end();
 }
 
 function upload(response) {
--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-  function onRequest(request, response) {
+  function onRequest(request, response) 
+  {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
     route(handle, pathname, response);
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(process.env.PORT);
   console.log("Server has started.");
 }
 
